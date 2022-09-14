a92de048863963282d0292b185836fff4020d3c1
ayzubair <ayzubair@hotmail.com>
2022-09-14 02:36:50 -0700

    betty coding style perfected

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main - prints the size of various types.
- * Description: using the main function, declare variables for the individual types and print their sizes using the printf function
+ * Description: using the main function, declare variables for the
+ * individual types and print their sizes using the printf function
  * Return:0
  */
 int main(void)
@@ -13,11 +14,11 @@ int main(void)
 	float f;
 
 
-	printf("Size of a char: %ld byte(s)\n" , sizeof(c));
-	printf("Size of an int: %ld byte(s)\n" , sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n" , sizeof(li));
-	printf("Size of a long long int: %ld byte(s)\n" , sizeof(lli));
-	printf("Size of a float: %ld byte(s)\n" , sizeof(f));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
 
