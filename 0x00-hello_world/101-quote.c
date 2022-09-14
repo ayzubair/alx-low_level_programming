258a802d0f39744a921ccee47361a8e036df9ba9
ayzubair <ayzubair@hotmail.com>
2022-09-14 04:22:33 -0700

    h

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,7 @@
 #include <unistd.h>
 /**
  *main - Write out some quoted texts.
- *Description: Using the main function this program outputs 
- *"and that piece of art is useful" - Dora Korpar, 2015-10-19".
+ *Description: Using the main function this program outputs some texts
  *
  * Return: 1
  */
