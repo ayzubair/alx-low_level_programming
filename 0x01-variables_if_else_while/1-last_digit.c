2061ca46e66d38953979ab1232e32fe62b7cd673
ayzubair <ayzubair@hotmail.com>
2022-09-16 01:05:18 -0700

    Added task 1-last_digit.c -- prints the last digit of a rand number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is %d and", n, m);
+	printf("Last digit of %d is %d and", n, n % 10);
 	if (n % 10 > 5)
 		printf("is greater than 5\n");
 	else if (n % 10 == 0)
